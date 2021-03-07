26f8d800207c85cdc865eea42ff9b1f2a333fdf4
HevinGolakiya <hevinmgolakiya4@gmail.com>
2021-03-07 10:43:34 +0530

    my commit

---
+++ app/src/main/java/hego/android/introsliders/Dashboard.java
@@ -0,0 +1,14 @@
+package hego.android.introsliders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
